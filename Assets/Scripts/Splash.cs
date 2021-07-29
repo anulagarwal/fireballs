27b595faece5df4e4e55620dcf8caab6634e2d58
Javasamurai <szsimplesyed93@gmail.com>
2021-07-29 23:21:38 +0530

    Splash and gameplay sceme. Added remaining folders

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Splash : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("LoadGamePlay", 1f);
+    }
+
+    void LoadGamePlay() {
+        SceneManager.LoadScene(1);
+    }
+}

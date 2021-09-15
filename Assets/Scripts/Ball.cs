a0b3746c571105460228febfc047287f95787df3
Javasamurai <szsimplesyed93@gmail.com>
2021-09-16 00:12:16 +0530

    Added dotween, fixed issue with multiple modifiers, added feedbacks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using DG.Tweening;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -19,24 +18,26 @@ public class Ball : MonoBehaviour
     }
 
     public void Shrink() {
-        transform.localScale -= transform.localScale * 0.1f;
-        if (transform.localScale.x <= 0) {
-            Destroy(gameObject);
-        }
+        transform.DOScale(transform.localScale.x - (transform.localScale.x * 0.1f), 0.2f).SetEase(Ease.InOutCubic).OnComplete(() => {
+            if (transform.localScale.x <= 0) {
+                Destroy(gameObject);
+            }
+        });
+        // transform.localScale -= transform.localScale * 0.1f;
 
         // Vector3.Lerp(transform.localScale, transform.localScale - transform.localScale * 0.5f, Time.deltaTime * 10);
     }
 
-    private void OnCollisionEnter(Collision other) {
-        if (!activated && (other.gameObject.CompareTag("Ball")) ) {
-            if (other.gameObject.GetComponent<Ball>().activated) {
-                activated = true;
-                meshRenderer.material = activatedMaterial;
-            }
-        } else if (other.gameObject.CompareTag("Head") && activated){
-            rigidbody.useGravity = false;
-            rigidbody.isKinematic = true;
-            other.gameObject.GetComponent<MeshRenderer>().material = activatedMaterial;
-        }
-    }
+    // private void OnCollisionEnter(Collision other) {
+    //     if (!activated && (other.gameObject.CompareTag("Ball")) ) {
+    //         if (other.gameObject.GetComponent<Ball>().activated) {
+    //             // activated = true;
+    //             meshRenderer.material = activatedMaterial;
+    //         }
+    //     } else if (other.gameObject.CompareTag("Head") && activated){
+    //         rigidbody.useGravity = false;
+    //         rigidbody.isKinematic = true;
+    //         other.gameObject.GetComponent<MeshRenderer>().material = activatedMaterial;
+    //     }
+    // }
 }
--- Assets/Scripts/BallModifier.cs
+++ Assets/Scripts/BallModifier.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using DG.Tweening;
+using System.Collections.Generic;
 
 public class BallModifier : MonoBehaviour
 {
@@ -22,18 +22,30 @@ public class BallModifier : MonoBehaviour
         DIVIDER
     }
 
+    private Tween scaleUpTween;
     [SerializeField]
     float value;
 
     [SerializeField]
     public MODIFIER_TYPE currentModifier = MODIFIER_TYPE.ADDER;
-    
+
+    List<Ball> spawnedBalls;
     private void Start() {
+        spawnedBalls = new List<Ball>();
         currentValLbl.text = currentModifier == MODIFIER_TYPE.ADDER ? "+" + value : "*" + value;
+        DOTween.defaultAutoKill = false;
     }
 
     async private void OnTriggerEnter(Collider other) {
-        if (other.transform.CompareTag("Ball") && !other.GetComponent<Ball>().activated && value > 0) {
+        if (other.transform.CompareTag("Ball")) {
+            Debug.LogError("Trigger");
+        }
+        if (other.transform.CompareTag("Ball") && !spawnedBalls.Contains(other.GetComponent<Ball>()) && value > 0) {
+            if (scaleUpTween == null || (scaleUpTween != null && !scaleUpTween.IsPlaying())) {
+                scaleUpTween = currentValLbl.transform.DOScale(currentValLbl.transform.localScale.x + 0.2f, 0.15f).SetEase(Ease.InOutCubic).OnComplete(() => {
+                    currentValLbl.transform.DOScale(currentValLbl.transform.localScale.x - 0.2f, 0.15f).SetEase(Ease.InOutCubic);
+                });
+            }
             if (currentModifier == MODIFIER_TYPE.ADDER) {
                 value -= 1;
                 currentValLbl.text = "+" + value;
@@ -50,9 +62,13 @@ public class BallModifier : MonoBehaviour
     }
 
     void SpawnBall(Collider colliderPos) {
-        Ball ball = Instantiate<Ball>(ballPrefab, colliderPos.transform.position, Quaternion.identity);
-        colliderPos.GetComponent<Ball>().activated = true;
-        ball.activated = true;
-        ball.transform.SetParent(ballContainer.transform);
+        if (colliderPos != null) {
+            Ball ball = Instantiate<Ball>(ballPrefab, colliderPos.transform.position, Quaternion.identity);
+
+            spawnedBalls.Add(ball);
+            // colliderPos.GetComponent<Ball>().activated = true;
+            // ball.activated = true;
+            ball.transform.SetParent(ballContainer.transform);
+        }
     }
 }
